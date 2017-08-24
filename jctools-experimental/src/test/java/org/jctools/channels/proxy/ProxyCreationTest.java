ec297b668e03fba41d17cc8d8ce0709465572130
Doug Lawrie <doug@neverfear.org>
2017-08-24 14:16:49 +0100

    WIP

--- jctools-experimental/src/test/java/org/jctools/channels/proxy/ProxyCreationTest.java
+++ jctools-experimental/src/test/java/org/jctools/channels/proxy/ProxyCreationTest.java
@@ -1,7 +1,14 @@
 package org.jctools.channels.proxy;
 
+import static java.lang.System.out;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.jctools.channels.WaitStrategy;
 import org.jctools.channels.mpsc.MpscOffHeapFixedSizeRingBuffer;
@@ -25,6 +32,15 @@ public class ProxyCreationTest {
 
     @Rule
     public ExpectedException expectedException = ExpectedException.none();
+    
+    static class ThreadInfo extends CustomType {
+        final int producerThread;
+        int lastSeenMessageIndex=-1;
+
+        public ThreadInfo(int producerThread) {
+            this.producerThread = producerThread;
+        }
+    }
 
     @Test
     public void testGeneratedProxyInstance() {
@@ -36,6 +52,114 @@ public class ProxyCreationTest {
          * types it should at least be a DemoIFace
          */
         assertThat(proxyChannel.proxyInstance(proxy), instanceOf(DemoIFace.class));
+    }
+
+    @Test
+    public void givenGeneratedProxyUsingMpscReferenceChannel_whenCallMethodsFromManyProducers_expectAllCallsAreProxied() throws Exception {
+        int producerThreads = 2;
+        int messageCount = 1_000_000;
+        
+        // +1 for consumer
+        CountDownLatch startLatch = new CountDownLatch(producerThreads + 1);
+        AtomicBoolean stop = new AtomicBoolean(false);
+        
+        Class<? extends ProxyChannelRingBuffer> backend = MpscOffHeapFixedSizeRingBuffer.class;
+        ProxyChannel<DemoIFace> proxyChannel =
+                ProxyChannelFactory.createProxy(10, DemoIFace.class, (idleCounter) -> {
+                    if (stop.get()) {
+                        throw new AssertionError();
+                    }
+                    return 0;
+                }, backend);
+        DemoIFace proxy = proxyChannel.proxy();
+
+        Thread[] threads = new Thread[producerThreads];
+        for (int producerThread = 0; producerThread < producerThreads; producerThread++) {
+            final int producerThreadIndex = producerThread;
+            Runnable runnable = () -> {
+                ThreadInfo threadInfo = new ThreadInfo(producerThreadIndex);
+                
+                startLatch.countDown();
+                try {
+                    startLatch.await();
+
+                    for (int messageIndex = 0; messageIndex < messageCount; messageIndex++) {
+                        proxy.call4(Integer.valueOf(messageIndex), threadInfo);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    out.println("Producer " + producerThreadIndex + " exiting");
+                }
+            };
+
+            threads[producerThread] = new Thread(runnable, ProxyCreationTest.class.getName() + "-" + producerThread);
+            threads[producerThread].setDaemon(true);
+            threads[producerThread].start();
+        }
+        
+        startLatch.countDown();
+        startLatch.await();
+        
+        int expectedMessageCount = producerThreads * messageCount;
+        int seenCount = 0;
+        try {
+            while (seenCount < expectedMessageCount) {
+                seenCount += proxyChannel.process(new DemoIFace() {
+
+                    @Override
+                    public void call4(Object x, CustomType y) {
+                        assertNotNull(x);
+                        assertNotNull(y);
+
+                        /*
+                         * This section should be uncommented once the multi producer issue whereby P1 can overwrite P0
+                         * are
+                         * fixed
+                         */
+                        // ThreadInfo info = (ThreadInfo) y;
+                        // int messageIndex = ((Integer) x).intValue();
+                        //
+                        // int nextMessageIndex = info.lastSeenMessageIndex + 1;
+                        // assertEquals("There was a gap in message indices for producer " + info.producerThread,
+                        // nextMessageIndex,
+                        // messageIndex);
+                        // info.lastSeenMessageIndex = messageIndex;
+                    }
+
+                    @Override
+                    public void call6(int x, CustomType[] y, CustomType... z) {
+                        throw new AssertionError();
+                    }
+
+                    @Override
+                    public void call5(CustomType x, int y, CustomType z) {
+                        throw new AssertionError();
+                    }
+
+                    @Override
+                    public void call3() {
+                        throw new AssertionError();
+                    }
+
+                    @Override
+                    public void call2(float x, double y, boolean z) {
+                        throw new AssertionError();
+                    }
+
+                    @Override
+                    public void call1(int x, int y) {
+                        throw new AssertionError();
+                    }
+                }, 1);
+            }
+        } finally {
+            stop.set(true);
+        }
+
+        for (int producerThread = 0; producerThread < producerThreads; producerThread++) {
+            threads[producerThread].join();
+        }
     }
 
     @Test
